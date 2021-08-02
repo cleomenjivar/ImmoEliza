e8a749276c0246688db26a1561c79c5d63a727d2
cleomenjivar <cleomenjivar@gmail.com>
2021-08-02 12:27:37 +0200

    Added the backend folder from Pauwel, testing.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello Pauwel
         </a>
       </header>
     </div>
